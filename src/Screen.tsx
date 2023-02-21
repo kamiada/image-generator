206bbe2177261a580b23f0c8cbd24d7d2558464e
kamiada <kamiada528@gmail.com>
2023-02-21 01:06:48 +0000

    initial

---
+++ src/Screen.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { FC, useEffect, useState } from 'react';
+
+interface Props {
+    onClick: () => void;
+}
+
+export const Screen: FC<Props> = () => {
+    const [data, setData] = useState();
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${}`;
+    useEffect(() => {
+        const getMyData = async () => {
+             fetch(url, {
+                headers: {
+                    "Accept": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            }).then(response => response.json()).then(data => setData(data));
+        }; getMyData();
+    }, []);
+    return (
+        <div>
+            <h1>get Data</h1>
+            <div>
+
+                {data && 
+                <div>
+                <img alt='nasa' src={data['url']} />
+                <p>{data['explanation']}</p>
+                </div>}
+            </div>
+        </div>
+
+    )
+}
